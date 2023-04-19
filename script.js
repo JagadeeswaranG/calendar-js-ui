b9ea742f6799667d486338aa05aad2a1a97e9047
JagadeeswaranG <jagadees.vg@gmail.com>
2023-04-19 16:28:19 +0530

    2x

--- script.js
+++ script.js
@@ -5,32 +5,146 @@ let currentYear = today.getFullYear();
 displayCalendar(currentMonth, currentYear);
 
 // Add event listeners for the navigation buttons and year select
-document.getElementById("prevMonth").addEventListener("click", () => {
-  if (currentMonth === 0) {
-    currentMonth = 11;
-    currentYear--;
-  } else {
-    currentMonth--;
-  }
+// document.getElementById("prevTwoMonth").addEventListener("click", () => {
+//   if (currentMonth === 0 ) {
+//     currentMonth = 10;
+//     currentYear--;
+//   } else {
+//     currentMonth = currentMonth - 2 ;
+//   }
 
-  displayCalendar(currentMonth, currentYear);
-});
+//   displayCalendar(currentMonth, currentYear);
+// });
 
-document.getElementById("nextMonth").addEventListener("click", () => {
-  if (currentMonth === 11) {
-    currentMonth = 0;
-    currentYear++;
-  } else {
-    currentMonth++;
-  }
-  displayCalendar(currentMonth, currentYear);
-});
+// document.getElementById("prevMonth").addEventListener("click", () => {
+//   if (currentMonth === 0) {
+//     currentMonth = 11;
+//     currentYear--;
+//   } else {
+//     currentMonth--;
+//   }
+
+//   displayCalendar(currentMonth, currentYear);
+// });
+
+// document.getElementById("nextMonth").addEventListener("click", () => {
+//   if (currentMonth === 11) {
+//     currentMonth = 0;
+//     currentYear++;
+//   } else {
+//     currentMonth++;
+//   }
+//   displayCalendar(currentMonth, currentYear);
+// });
+
+// document.getElementById("nextTwoMonth").addEventListener("click", () => {
+//   if (currentMonth === 11) {
+//     currentMonth = 1;
+//     currentYear++;
+//   } else {
+//     currentMonth = currentMonth + 2 ;
+//   }
+//   displayCalendar(currentMonth, currentYear);
+// });
 
 document.getElementById("yearSelect").addEventListener("change", () => {
   currentYear = parseInt(document.getElementById("yearSelect").value);
   displayCalendar(currentMonth, currentYear);
 });
 
+//Prev Month Selection
+document.getElementById("prevMon").addEventListener("change", () => {
+  currentSelect = document.getElementById("prevMon").value;
+  // console.log(currentMonth);
+
+  if (currentSelect === "x") {
+    if (currentMonth === 0) {
+      currentMonth = 11;
+      currentYear--;
+    } else {
+      currentMonth--;
+    }
+    displayCalendar(currentMonth, currentYear);
+  }
+
+  if (currentSelect === "2x") {
+    if (currentMonth <= 1) {
+      currentMonth = 12 + currentMonth - 2;
+      currentYear--;
+    } else {
+      currentMonth -= 2;
+    }
+    displayCalendar(currentMonth, currentYear);
+  }
+
+  if (currentSelect === "4x") {
+    if (currentMonth <= 3) {
+      currentMonth = 12 + currentMonth - 4;
+      currentYear--;
+    } else {
+      currentMonth -= 4;
+    }
+    displayCalendar(currentMonth, currentYear);
+  }
+
+  if (currentSelect === "6x") {
+    if (currentMonth <= 5) {
+      currentMonth = 12 + currentMonth - 6;
+      currentYear--;
+    } else {
+      currentMonth -= 6;
+    }
+    displayCalendar(currentMonth, currentYear);
+  }
+});
+
+// Next Month Selection
+document.getElementById("nextMon").addEventListener("change", () => {
+  currentSelect = document.getElementById("nextMon").value;
+  // console.log(currentMonth);
+
+  if (currentSelect === "x") {
+    if (currentMonth === 11) {
+      currentMonth = 0;
+      currentYear++;
+    } else {
+      currentMonth++;
+    }
+    displayCalendar(currentMonth, currentYear);
+  }
+
+  if (currentSelect === "2x") {
+    if (currentMonth >= 10) {
+      currentMonth = currentMonth + 2 - 12;
+      currentYear++;
+    } else {
+      currentMonth += 2;
+    }
+    displayCalendar(currentMonth, currentYear);
+  }
+
+  if (currentSelect === "4x") {
+    if (currentMonth >= 8) {
+      currentMonth = currentMonth + 4 - 12;
+      currentYear++;
+    } else {
+      currentMonth += 4;
+    }
+    displayCalendar(currentMonth, currentYear);
+  }
+
+  if (currentSelect === "6x") {
+    if (currentMonth >= 6) {
+      currentMonth = currentMonth + 6 - 12;
+      currentYear++;
+    } else {
+      currentMonth += 6;
+    }
+    displayCalendar(currentMonth, currentYear);
+  }
+});
+
+let allEvents = [];
 function displayCalendar(month, year) {
   // Get a reference to the calendar container
   const calendarContainer = document.getElementById("calendar");
@@ -114,17 +228,29 @@ function displayCalendar(month, year) {
     // Add a click event listener to the cell to display a message when clicked
     cell.addEventListener("click", () => {
       // alert(`You clicked on ${i}/${month + 1}/${year}`);
+
       let userEvent = prompt(
         `Add your Event on ${i}/${month + 1}/${year}`,
         "Add here !"
       );
       console.log(userEvent);
-      if (userEvent && userEvent !== null) {
-        cell.innerHTML = `${i}<br><br><div id="eventList">${userEvent}</div> `;
-      } else {
-        cell.innerHTML = i;
-      }
+      allEvents.push({ id: `${i}${month + 1}${year}`, eve: userEvent });
+      console.log(allEvents);
+      allEvents.forEach((item) => {
+        if (item.id == `${i}${month + 1}${year}` && item.eve !== null) {
+          cell.innerHTML = `${i}<br><br><div id="eventList">${item.eve}</div> `;
+        } else {
+          cell.innerHTML = i;
+        }
+      });
     });
+
+    //   if (userEvent && userEvent !== null) {
+    //     cell.innerHTML = `${i}<br><br><div id="eventList">${userEvent}</div> `;
+    //   } else {
+    //     cell.innerHTML = i;
+    //   }
+    // });
 
     // Add the cell to the current row
     currentRow.appendChild(cell);
